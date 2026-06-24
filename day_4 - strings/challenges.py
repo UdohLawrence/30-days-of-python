d242bf1e1736c511110c672aeeed8590a02bdb3a
UdohLawrence <lawrenceaudoh@gmail.com>
2026-06-24 21:40:49 +0100

    provide solutions for questions 1 - 4

---
+++ day_4 - strings/challenges.py
@@ -0,0 +1,36 @@
+# # To check the length of a string, use the len() method
+# letter = 'p'
+# print(letter)
+# print(len(letter))
+# greeting = "Hello, World!"
+# print(greeting)
+# print(len(greeting))
+# sentence = "I hope you are enjoying 30 days of Python"
+# print(sentence)
+# print(len(sentence))
+
+# # Multiline String
+# multiline_string = """
+#   I am a teacher, and I enjoy teaching.
+#   I am yet to find anything as rewarding as empowering people.
+#   That is why I have decided to volunteer for this project.
+# """
+# print(multiline_string)
+
+# # String Concatenation
+# first_name = "Ekomobong"
+# last_name = "Lawrence"
+# space = " "
+# full_name = first_name + space + last_name
+# print(full_name)
+# print(len(first_name))
+# print(len(last_name))
+# print(len(full_name))
+# print(len(first_name) > len(last_name))
+
+# Escape Sequences
+# \n: new line
+# \t: Tab (8 spaces)
+# \\: Back slash
+# \': Single quote
+# \": Double quote
---
+++ day_4 - strings/strings.py
@@ -0,0 +1,78 @@
+# Question 1
+space = ' '
+thirty_days_of_python = 'Thirty' + space + 'Days' + space + 'Of' + space + 'Python'
+print(thirty_days_of_python)
+print()
+
+#Question 2
+coding_for_all = 'Coding' + space + 'For' + space + 'All'
+print(coding_for_all)
+print()
+
+#Question 3
+company = 'Coding For All'
+
+# Question 4
+print(company)
+print()
+
+# Question 5
+print(len(company))
+print()
+
+# Question 6
+company = company.upper()
+print(company)
+print()
+
+# Question 7
+company = company.lower()
+print(company)
+print()
+
+# Question 8
+print("Coding For All".capitalize())
+print("Coding For All".title())
+print("Coding For All".swapcase())
+print()
+
+#Question 9
+print("Coding For All"[7::])
+print()
+
+#Question 10
+print("Coding For All".index("Coding"))
+print()
+
+# Question 11
+print("Coding For All".replace("Coding", "Python"))
+print()
+
+# Question 12
+print("Python For Everyone".replace("Everyone", "All"))
+print()
+
+# Question 13
+split_string = "Coding For All".split(' ')
+print(split_string)
+print()
+
+# Question 14
+big_companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(big_companies.split(', '))
+print()
+
+# Question 15
+coding_for_all = 'Coding For All'
+print(coding_for_all[0])
+print()
+
+# Question 16
+print(len("Coding For All") - 1)
+print()
+
+# Question 17
+print("Coding For All"[10])
+print()
+
+# Question 18
